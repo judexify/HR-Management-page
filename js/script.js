c38f1ab53a413dbbd793a7435819d9afd4b899c6
Judexify <olukojuoluwadunsi12@gmail.com>
2025-12-23 05:46:01 +0100

    fixed login to some extent

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 "use strict";
 
 const body = document.body;
-const passwordInput = document.querySelector("#password");
-const seePasswordBtn = document.querySelector("#checkbox");
 
-function isMobile() {
+const seePasswordBtn = document.querySelector("#checkbox");
+const email = document.querySelector(".email");
+const password = document.querySelector(".password");
+
+const isMobile = () => {
   const minWidth = 768; // Minimum width for desktop devices
   return window.innerWidth < minWidth || screen.width < minWidth;
-}
+};
 
 document.addEventListener("DOMContentLoaded", function () {
   if (isMobile()) {
@@ -40,10 +42,51 @@ document.addEventListener("DOMContentLoaded", function () {
 
 let isChecked = false;
 const forPasswordInput = () => {
+  const passwordInput = document.querySelector(".password");
   passwordInput.type = isChecked ? "text" : "password";
 };
 
 seePasswordBtn.addEventListener("change", () => {
   isChecked = !isChecked;
   forPasswordInput();
+});
+
+// function for validation of forms
+
+const handleEmailValidity = () => {
+  const emailRegExp = /^[\w.!#$%&'+/=?^`{|}~-]+@[a-z\d-]+(?:\.[a-z\d-]+)$/i;
+  const emailValidty =
+    email.value.length !== 0 && emailRegExp.test(email.value);
+  visualForErrorEmail(emailValidty);
+};
+
+const visualForErrorEmail = (emailValidty) => {
+  if (!emailValidty) {
+    email.setAttribute("required", true);
+    email.classList.add("error");
+    email.classList.remove("valid");
+  } else {
+    email.classList.remove("error");
+    email.classList.add("valid");
+  }
+};
+const handlePassValidity = () => {
+  const passRegExp = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*_)(?!.* ).{5,16}$/;
+  const passValidty =
+    password.value.length > 5 && passRegExp.test(password.value);
+
+  if (!passValidty) {
+    password.classList.add("error");
+    password.classList.remove("valid");
+  } else {
+    password.classList.remove("error");
+    password.classList.add("valid");
+  }
+};
+email.addEventListener("input", () => {
+  handleEmailValidity();
+});
+
+password.addEventListener("input", () => {
+  handlePassValidity();
 });
