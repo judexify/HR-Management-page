d627ecd2ff5aa8d7561126e684744449654cd898
Judexify <olukojuoluwadunsi12@gmail.com>
2025-12-24 16:56:46 +0100

    fixed calendars

--- js/loggedInPage.js
+++ js/loggedInPage.js
@@ -1,1 +1,105 @@
 "use strict";
+
+const daysContainer = document.querySelector("#days");
+const monthYear = document.querySelector("#month-year");
+const prev = document.querySelector("#prev");
+const next = document.querySelector("#next");
+console.log(next);
+
+const renderCalendar = (date) => {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const year = date.getFullYear();
+  const month = date.getMonth();
+
+  const firstDay = new Date(year, month, 1).getDay();
+  const lastDay = new Date(year, month + 1, 0).getDate();
+
+  monthYear.textContent = `${months[month]} ${year}`;
+
+  daysContainer.innerHTML = "";
+
+  previousMonthDays(year, month, firstDay);
+  renderDays(lastDay, month, year);
+  nextMonthDays(year, month);
+};
+
+const previousMonthDays = (year, month, firstDay) => {
+  const prevMonthLastDay = new Date(year, month, 0).getDate();
+
+  for (let i = firstDay; i > 0; i--) {
+    const dayDiv = document.createElement("div");
+    dayDiv.textContent = prevMonthLastDay - i + 1;
+    dayDiv.classList.add("fade");
+    daysContainer.appendChild(dayDiv);
+  }
+};
+
+const renderDays = (lastDay, month, year) => {
+  const today = new Date();
+
+  for (let i = 1; i <= lastDay; i++) {
+    const dayDiv = document.createElement("div");
+    dayDiv.textContent = i;
+    dayDiv.setAttribute("tabindex", "0");
+
+    if (
+      i === today.getDate() &&
+      month === today.getMonth() &&
+      year === today.getFullYear()
+    ) {
+      dayDiv.classList.add("today");
+    }
+
+    daysContainer.appendChild(dayDiv);
+  }
+};
+
+const nextMonthDays = (year, month) => {
+  const lastDayIndex = new Date(year, month + 1, 0).getDay();
+  const trailingDays = 6 - lastDayIndex;
+
+  for (let i = 1; i <= trailingDays; i++) {
+    const dayDiv = document.createElement("div");
+    dayDiv.textContent = i;
+    dayDiv.classList.add("fade");
+    daysContainer.appendChild(dayDiv);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  let currentDate = new Date();
+
+  prev.addEventListener("click", function () {
+    previousMonth(currentDate);
+  });
+
+  next.addEventListener("click", function (e) {
+    nextMonth(currentDate);
+  });
+
+  renderCalendar(currentDate);
+});
+
+const nextMonth = (currentDate) => {
+  currentDate.setMonth(currentDate.getMonth() + 1);
+  renderCalendar(currentDate);
+};
+
+const previousMonth = (currentDate) => {
+  currentDate.setMonth(currentDate.getMonth() - 1);
+  renderCalendar(currentDate);
+};
